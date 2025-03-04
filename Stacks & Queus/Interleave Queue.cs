29190827e1c8b1659e85ff602cfb632e7bcd0b9e
Youssif <youssefbro12397@gmail.com>
2025-03-04 04:39:59 +0200

    Interleave Queue

---
+++ Stacks & Queus/Interleave Queue.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void InterleaveQueue(Queue<int> queue) 
+    {
+        int halfSize = queue.Count / 2;
+        Queue<int> firstHalf = new Queue<int>();
+ 
+        for (int i = 0;i < halfSize;i++)
+        {
+            firstHalf.Enqueue(queue.Dequeue());
+        }
+        for (int i = 0; i < halfSize; i++)
+        {
+            queue.Enqueue(firstHalf.Dequeue());
+            queue.Enqueue(queue.Dequeue());
+        }
+    }
+    static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>(new int[] { 1, 2, 3, 4, 5, 6 });
+         InterleaveQueue(queue);
+        Console.WriteLine(string.Join(", ",queue));
+        Console.ReadKey();
+    }
+}
+
